c85c1e4b3a98e703fe7371e8d48fad749ea36073
hodisan mihai <lostmemory.ro@gmail.com>
2024-07-26 21:15:46 +0300

    dropdown

---
+++ home.js
@@ -0,0 +1,42 @@
+document.getElementById('filterToggleButton').addEventListener('click', function() {
+    document.getElementById('filterOptions').classList.toggle('show');
+});
+
+function filterItems(criteria) {
+    const items = document.querySelectorAll('.most-item');
+    items.forEach(item => {
+        const categories = item.dataset.category.split(' ');
+        if (criteria === 'all' || categories.includes(criteria)) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+}
+
+document.querySelectorAll('.most button').forEach(button => {
+    button.addEventListener('mouseover', function() {
+        this.closest('.most-item').classList.add('most-item-hover');
+    });
+    button.addEventListener('mouseout', function() {
+        this.closest('.most-item').classList.remove('most-item-hover');
+    });
+});
+
+const optionMenu = document.querySelector(".select-menu"),
+    selectBtn = optionMenu.querySelector(".select-btn"),
+    options = optionMenu.querySelectorAll(".option"),
+    sBtn_text = optionMenu.querySelector(".sBtn-text");
+
+selectBtn.addEventListener("click", () => {
+    optionMenu.classList.toggle("active");
+});
+
+options.forEach(option => {
+    option.addEventListener("click", () => {
+        let selectedOption = option.querySelector(".option-text").innerText;
+        sBtn_text.innerText = selectedOption;
+
+        optionMenu.classList.remove("active");
+    });
+});
